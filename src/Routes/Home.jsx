89c89b03e7ff716b3da3878ed6c405d609a9a153
fauzanpr <fauzanpr.06@gmail.com>
2023-06-21 11:00:52 +0700

    feat: using Chakra UI and implement button in home page (20 passed)

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,12 @@
 // TODO: answer here
 import { Link } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <>
       <Link data-testid="student-btn" to={"/student"}>
-        All Student
+        <Button colorScheme="blue">All Student</Button>
       </Link>
     </>
   ); // TODO: replace this
