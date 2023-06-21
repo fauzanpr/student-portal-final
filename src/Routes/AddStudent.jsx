df6874be8cd982dc8895d9b11d9ce7ff8413eca9
fauzanpr <fauzanpr.06@gmail.com>
2023-06-21 19:11:22 +0700

    feat: add styling on add student (26 passed)

--- src/Routes/AddStudent.jsx
+++ src/Routes/AddStudent.jsx
@@ -2,6 +2,15 @@
 import { useState } from "react";
 import NavBar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  FormLabel,
+  Input,
+  Select,
+  Container,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
 
 const AddStudent = () => {
   // TODO: answer here
@@ -72,97 +81,118 @@ const AddStudent = () => {
     <>
       {/* TODO: answer here */}
       <NavBar />
-      <p>Halaman add student</p>
-      <form onSubmit={(e) => submitHandler(e)}>
-        <div>
-          <label htmlFor="fullname">Fullname</label>
-          <input
-            type="text"
-            data-testid="name"
-            value={fullname}
-            onChange={(e) => setFullname(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="profilePicture">Profile Picture</label>
-          <input
-            type="text"
-            data-testid="profilePicture"
-            value={profilePicture}
-            onChange={(e) => setProfilePicture(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            data-testid="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="phoneNumber">Phone Number</label>
-          <input
-            type="text"
-            data-testid="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="date">Birth Date</label>
-          <input
-            type="date"
-            data-testid="date"
-            value={birthDate}
-            onChange={(e) => setBirthDate(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="gender">Gender</label>
-          <select
-            name="gender"
-            data-testid="gender"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-          >
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="programStudy">Program Study</label>
-          <select
-            name="programStudy"
-            id="input-prody"
-            data-testid="prody"
-            value={programStudy}
-            onChange={(e) => setProgramStudy(e.target.value)}
-          >
-            <option value="Ekonomi">Ekonomi</option>
-            <option value="Manajemen">Manajemen</option>
-            <option value="Akuntansi">Akuntansi</option>
-            <option value="Administrasi Publik">Administrasi Publik</option>
-            <option value="Administrasi Bisnis">Administrasi Bisnis</option>
-            <option value="Hubungan Internasional">
-              Hubungan Internasional
-            </option>
-            <option value="Teknik Sipil">Teknik Sipil</option>
-            <option value="Arsitektur">Arsitektur</option>
-            <option value="Matematika">Matematika</option>
-            <option value="Fisika">Fisika</option>
-            <option value="Informatika">Informatika</option>
-          </select>
-        </div>
-        <input
+      <Heading
+        as="h2"
+        textAlign="center"
+        mb="2rem"
+        fontSize="1.5rem"
+        color="blue.600"
+      >
+        Halaman add student
+      </Heading>
+      <Container>
+        <form onSubmit={(e) => submitHandler(e)}>
+          <Flex direction="column" gap="1rem">
+            <div>
+              <FormLabel htmlFor="fullname">Fullname</FormLabel>
+              <Input
+                type="text"
+                data-testid="name"
+                value={fullname}
+                onChange={(e) => setFullname(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="profilePicture">Profile Picture</FormLabel>
+              <Input
+                type="text"
+                data-testid="profilePicture"
+                value={profilePicture}
+                onChange={(e) => setProfilePicture(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="address">Address</FormLabel>
+              <Input
+                type="text"
+                data-testid="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+              <Input
+                type="text"
+                data-testid="phoneNumber"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="date">Birth Date</FormLabel>
+              <Input
+                type="date"
+                data-testid="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="gender">Gender</FormLabel>
+              <Select
+                name="gender"
+                data-testid="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </Select>
+            </div>
+            <div>
+              <FormLabel htmlFor="programStudy">Program Study</FormLabel>
+              <Select
+                name="programStudy"
+                id="input-prody"
+                data-testid="prody"
+                value={programStudy}
+                onChange={(e) => setProgramStudy(e.target.value)}
+              >
+                <option value="Ekonomi">Ekonomi</option>
+                <option value="Manajemen">Manajemen</option>
+                <option value="Akuntansi">Akuntansi</option>
+                <option value="Administrasi Publik">Administrasi Publik</option>
+                <option value="Administrasi Bisnis">Administrasi Bisnis</option>
+                <option value="Hubungan Internasional">
+                  Hubungan Internasional
+                </option>
+                <option value="Teknik Sipil">Teknik Sipil</option>
+                <option value="Arsitektur">Arsitektur</option>
+                <option value="Matematika">Matematika</option>
+                <option value="Fisika">Fisika</option>
+                <option value="Informatika">Informatika</option>
+              </Select>
+            </div>
+            <Button
+              type="submit"
+              value="Add student"
+              id="add-btn"
+              className="submit"
+              data-testid="add-btn"
+            >
+              Add Student
+            </Button>
+          </Flex>
+          {/* <input
           type="submit"
           value="Add student"
           id="add-btn"
           className="submit"
           data-testid="add-btn"
-        />
-      </form>
+        /> */}
+        </form>
+      </Container>
     </>
   );
 };
